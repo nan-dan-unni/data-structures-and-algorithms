17238343fd508ff9beb9b27fb96b247c4ac1e895
nan-dan-unni <asnqln@gmail.com>
2025-08-09 08:48:57 +0530

    [09-Aug-2025] max diff subarray

--- algorithms/arrays/max_diff_subarray.py
+++ algorithms/arrays/max_diff_subarray.py
@@ -1,22 +1,16 @@
-# best time to buy and sell stock
-
 def best_time_to_buy_and_sell_stock(arr: list[int]) -> list[int]:
-    current_diff = 0
+    max_diff = 0
     current_diff_start = 0
-    max_diff = current_diff
     max_diff_pos = [0, 0]
     for i in range(1, len(arr)):
         new_diff = arr[i] - arr[current_diff_start]
-        if new_diff < current_diff:
-            current_diff = 0
+        if arr[i] < arr[current_diff_start]:
             current_diff_start = i
-        else:
-            current_diff = new_diff
-
-        if current_diff > max_diff:
-            max_diff = current_diff
+        elif new_diff > max_diff:
             max_diff_pos = [current_diff_start, i]
+            max_diff = new_diff
     return max_diff, max_diff_pos
 
+
 if __name__ == "__main__":
-    print(best_time_to_buy_and_sell_stock([5,3,2,6,8,5]))
+    print(best_time_to_buy_and_sell_stock([7,1,5,3,6,4]))
